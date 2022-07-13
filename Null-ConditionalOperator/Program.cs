2642c86097c446c09c637ebd75781bd148c1e9de
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-13 12:29:25 +0200

    chaining

--- Null-ConditionalOperator/Program.cs
+++ Null-ConditionalOperator/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//https://www.youtube.com/watch?v=eOpmjGF4qxo
 namespace NullConditionalOperator
 {
     class Program
@@ -16,8 +16,24 @@ namespace NullConditionalOperator
         }
         static void Main(string[] args)
         {
-            Person person = null;
-            Console.WriteLine(person?.Name);
+            Person person = new Person
+            {
+                Name = "Gerald",
+                Partner = new Person
+                {
+                    Name = "Gerald2"
+                },
+            };
+
+            if (string.IsNullOrWhiteSpace(person?.Partner?.Name))
+            {
+                Console.WriteLine(person?.Partner?.Name == null); // result is bool
+                Console.WriteLine("A person has no name.");
+            }
+
+            Console.WriteLine(person?.Name); // if person is null then it will be
+                                             // skipped
+            Console.WriteLine(person?.Partner.Name);
         }
     }
 }
