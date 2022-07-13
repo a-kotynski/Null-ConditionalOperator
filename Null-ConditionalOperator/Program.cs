42a3aaa7d1cab7a6d95272e795d73a09f8aab695
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-13 11:22:02 +0200

    Add project files.

---
+++ Null-ConditionalOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullConditionalOperator
+{
+    class Program
+    {
+        public class Person // class Person inside class Program????
+        {
+            public string Name { get; set; }
+            public Person Partner { get; set; }
+            public string[] Hobbies { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            { // object initialization syntax?
+                Name = "Gerald" 
+            };
+            if (person != null)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+    }
+}
